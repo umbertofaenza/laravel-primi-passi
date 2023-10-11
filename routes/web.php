84b2d173305f2c177f3e39fb9d1a7447206c52d4
umbertofaenza <umberto.faenza@gmail.com>
2023-10-11 17:09:16 +0200

    feat: navbar with links to other pages

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,12 @@ Route::get('/', function () {
     return view('home', [
         'hello_msg' => $hello_msg
     ]);
-});
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
